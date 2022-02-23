2aa86db38956f12c16cd83aab51e88170c946dff
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-02-23 15:05:30 -0500

    Controller and routes created. still WIP

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,33 @@
 class BookingsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_booking, only: [:show, :update, :new]
-
-  def show; end
+  before_action :set_booking, only: [:destroy]
 
   def new
     @booking = Booking.new
-
+    set_pet
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.user = current_user
 
+    # @pet = Pet.find(params[:pet_id])
+    @booking.pet = set_pet
+    @booking.user = current_user
+    # raise
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to user_bookings_path(current_user)
     else
       render 'bookings/new'
     end
   end
+
+  def index
+    user_bookings
+  end
+
+  def show; end
+
 
   def user_bookings
     @user_bookings = Booking.where(user_id: current_user.id)
@@ -27,7 +35,7 @@ class BookingsController < ApplicationController
 
   def update
     if @booking.update(booking_params)
-      redirect_to @booking, notice: "The booking was confirmed successfully."
+      redirect_to @booking, notice: "You have booked this pet!."
     else
       render :edit
     end
@@ -36,11 +44,20 @@ class BookingsController < ApplicationController
   private
 
   def set_booking
+    raise
     @booking = Booking.find(params[:id])
   end
 
+  def set_user
+    @user = current_user
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  end
+
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :status)
+    params.require(:booking).permit(:start_date, :end_date, :pet_id, :user_id, :status)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,37 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
+    resources :bookings
   end
-	resources :pets do
+  resources :pets do
+    resources :bookings
   end
-  resources :bookings, only: [:new, :create, :destroy]
-
-  # get '/bookings/new', to: 'bookings#new'
-  # post '/bookings', to: 'bookings#create'
-
-  # get '/bookings', to: 'bookings#show'
 
   get '/profile', to: 'users#show'
 
+  # get '/bookings/new', to: 'bookings#new', as: :new_booking
+  # post '/bookings', to: 'bookings#create'
+  # get '/bookings/:id', to: 'bookings#show', as: :booking
+  # get '/bookings', to: 'bookings#index'
+  # get '/bookings/:id/edit', to: 'bookings#edit'
+  # patch '/bookings/:id', to: 'bookings#update'
+  # delete '/bookings/:id', to: 'bookings#destroy'
+
+
+  # get '/users/new', to: 'users#new', as: :new_user
+  # post '/users', to: 'users#create'
+  # get '/users/:id', to: 'users#show', as: :user
+  # get '/users', to: 'users#index'
+  # get '/users/:id/edit', to: 'users#edit'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
+
+  # get '/pets/new', to: 'pets#new', as: :new_pet
+  # post '/pets', to: 'pets#create'
+  # get '/pets/:id', to: 'pets#show', as: :pet
+  # get '/pets', to: 'pets#index'
+  # get '/pets/:id/edit', to: 'pets#edit'
+  # patch '/pets/:id', to: 'pets#update'
+  # delete '/pets/:id', to: 'pets#destroy'
 
 end
