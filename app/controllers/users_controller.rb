9a574f4ca4736a647d7df4c76d2afecebc115c43
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-02-24 10:13:52 -0500

    css and stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,18 @@ class UsersController < ApplicationController
 
   def show
     @username = current_user.email
+    user_pets
   end
 
   private
+
+  def user_bookings
+    @user_bookings = Booking.where(user_id: current_user.id)
+  end
+
+  def user_pets
+    @pets = Pet.where(user_id: current_user.id)
+  end
 
   def user_params
     params.require(:user).permit(:email, :img_url)
