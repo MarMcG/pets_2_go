5d088981bf9b81b87d94579ef756f4c835b00e90
Sedrak Nadzharyan <sedrak005@gmail.com>
2022-02-25 15:59:37 -0500

    before pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index]
   def home
-    @pets = Pet.all.shuffle.first(3)
+    @pets = Pet.all.shuffle.first(4)
   end
 end
