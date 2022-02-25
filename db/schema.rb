5b7032f525b12a4fbf223cccef55c5b7a28bd99c
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-02-25 10:54:16 -0500

    seedfile with users and pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-# ActiveRecord::Schema.define(version: 2022_02_24_164155) do
-
 ActiveRecord::Schema.define(version: 2022_02_24_172546) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
